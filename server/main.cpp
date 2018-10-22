c5552e1f23e33efbd422b447570292cf7ba684de
David Gu <david.gu@hotmail.com>
2018-10-22 19:33:38 -0400

    Added get_fps function, created constants for cameras

--- server/main.cpp
+++ server/main.cpp
@@ -1,13 +1,40 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <time.h>
+
 using namespace cv;
+
+const int CAMERA_1 = 0;
+const int CAMERA_2 = 1;
+const int CAMERA_3 = 2;
+
+double get_fps(VideoCapture cap){
+    int num_frames = 120;
+    time_t start, end;
+    Mat frame;
+
+    // Time capturing specified number of frames
+    time(&start);
+    for(int i = 0; i < num_frames; i++)
+    {
+        cap >> frame;
+    }
+    time(&end);
+     
+    // Calculate FPS
+    double seconds = difftime (end, start);
+    return num_frames / seconds;
+}
+
 int main(int argc, char** argv){
   VideoCapture cap;
-  if(!cap.open(1))
+  if(!cap.open(CAMERA_1))
     return 0;
 
   cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
   cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
+
+  std::cout << "FPS: " << get_fps(cap) << std::endl;
 
   while(true){
     Mat frame;
