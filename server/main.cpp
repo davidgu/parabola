5fcfa1d3da0ce922c94a719f4de11562eac15798
David Gu <david.gu@hotmail.com>
2018-10-22 19:49:33 -0400

    Implemented tracker selection

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/opencv.hpp"
+#include "opencv2/tracking.hpp"
 #include <iostream>
 #include <time.h>
 
@@ -7,6 +8,9 @@ using namespace cv;
 const int CAMERA_1 = 0;
 const int CAMERA_2 = 1;
 const int CAMERA_3 = 2;
+
+const std::string trackerList[8] = {"BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT"};
+const int trackerSelection = 2;
 
 double get_fps(VideoCapture cap){
     int num_frames = 120;
@@ -24,6 +28,28 @@ double get_fps(VideoCapture cap){
     // Calculate FPS
     double seconds = difftime (end, start);
     return num_frames / seconds;
+}
+
+Ptr<Tracker> get_tracker(std::string trackerType){
+  // Smart pointer to Tracker object
+  Ptr<Tracker> tracker;
+  if (trackerType == "BOOSTING")
+      tracker = TrackerBoosting::create();
+  if (trackerType == "MIL")
+      tracker = TrackerMIL::create();
+  if (trackerType == "KCF")
+      tracker = TrackerKCF::create();
+  if (trackerType == "TLD")
+      tracker = TrackerTLD::create();
+  if (trackerType == "MEDIANFLOW")
+      tracker = TrackerMedianFlow::create();
+  if (trackerType == "GOTURN")
+      tracker = TrackerGOTURN::create();
+  if (trackerType == "MOSSE")
+      tracker = TrackerMOSSE::create();
+  if (trackerType == "CSRT")
+      tracker = TrackerCSRT::create();
+  return tracker;
 }
 
 int main(int argc, char** argv){
