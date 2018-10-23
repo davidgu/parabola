e158774af15f2e2f88f25563f34c1c1748fe80eb
Curtis Chong <curtischong5@gmail.com>
2018-10-22 21:36:23 -0400

    added feature flag for threads

--- server/main.cpp
+++ server/main.cpp
@@ -9,48 +9,65 @@ using namespace cv;
 const int CAMERA_1 = 0;
 const int CAMERA_2 = 1;
 const int CAMERA_3 = 2;
+const int USE_THREADS = 0;
 
 const std::string trackerList[8] = {"BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT"};
 const int trackerSelection = 4;
 
 void get_fps(VideoCapture cap){
-    int num_frames = 120;
-    time_t start, end;
-    Mat frame;
+  int num_frames = 120;
+  time_t start, end;
+  Mat frame;
 
-    // Time capturing specified number of frames
-    time(&start);
-    for(int i = 0; i < num_frames; i++)
-    {
-        cap >> frame;
-    }
-    time(&end);
-     
-    // Calculate FPS
-    double seconds = difftime (end, start);
-    std::cout<< "FPS: " << num_frames / seconds << std::endl;
+  // Time capturing specified number of frames
+  time(&start);
+  for(int i = 0; i < num_frames; i++)
+  {
+    cap >> frame;
+  }
+  time(&end);
+
+  // Calculate FPS
+  double seconds = difftime (end, start);
+  std::cout<< "FPS: " << num_frames / seconds << std::endl;
 }
 
 Ptr<Tracker> get_tracker(std::string trackerType){
   // Smart pointer to Tracker object
   Ptr<Tracker> tracker;
   if (trackerType == "BOOSTING")
-      tracker = TrackerBoosting::create();
+    tracker = TrackerBoosting::create();
   if (trackerType == "MIL")
-      tracker = TrackerMIL::create();
+    tracker = TrackerMIL::create();
   if (trackerType == "KCF")
-      tracker = TrackerKCF::create();
+    tracker = TrackerKCF::create();
   if (trackerType == "TLD")
-      tracker = TrackerTLD::create();
+    tracker = TrackerTLD::create();
   if (trackerType == "MEDIANFLOW")
-      tracker = TrackerMedianFlow::create();
+    tracker = TrackerMedianFlow::create();
   if (trackerType == "GOTURN")
-      tracker = TrackerGOTURN::create();
+    tracker = TrackerGOTURN::create();
   if (trackerType == "MOSSE")
-      tracker = TrackerMOSSE::create();
+    tracker = TrackerMOSSE::create();
   if (trackerType == "CSRT")
-      tracker = TrackerCSRT::create();
+    tracker = TrackerCSRT::create();
   return tracker;
+}
+
+void process_frame(VideoCapture cap, Tracker* tracker){
+  Mat frame;
+  cap>>frame;
+  Rect2d bbox(287, 23, 86, 320);
+  // If tracking is successful, draw the bounding box
+  bool ok = tracker->update(frame, bbox);
+  if(ok){
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+  }
+  else{
+    std::cout << "Tracking failure!" << std::endl;
+  }
+
+  imshow("Tracking", frame);
 }
 
 int main(int argc, char** argv){
@@ -74,19 +91,24 @@ int main(int argc, char** argv){
   imshow("Tracking", frame);
 
   while(true){
-    cap >> frame;
-
-    // If tracking is successful, draw the bounding box
-    bool ok = tracker->update(frame, bbox);
-    if(ok){
-      rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
-    }
-    else{
-      std::cout << "Tracking failure!" << std::endl;
-    }
-
-    imshow("Tracking", frame);
     if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+    if(USE_THREADS){
+      std::thread t2(process_frame,cap,tracker);
+      t2.detach();
+    }else{
+      cap >> frame;
+
+      // If tracking is successful, draw the bounding box
+      bool ok = tracker->update(frame, bbox);
+      if(ok){
+        rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+      }
+      else{
+        std::cout << "Tracking failure!" << std::endl;
+      }
+
+      imshow("Tracking", frame);
+    }
   }
   // the camera will be closed automatically upon exit
   // cap.close();
