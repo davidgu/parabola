daa82ab36a03dbe9453a46857f0e063ef03ebb29
Curtis Chong <curtischong5@gmail.com>
2018-10-24 22:21:55 -0400

    fixed timing. created the frame collection code

--- collect_data/capture.cpp
+++ collect_data/capture.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
-#include <sys/time.h>
+#include <time.h>
 #include "opencv2/opencv.hpp"
 #define ll long long
- // note: I am loading it in with open cv because to maintain consistant image quality
+// note: I am loading it in with open cv because to maintain consistant image quality
 using namespace cv; 
-
+long timediff(clock_t t1, clock_t t2) {
+  long elapsed;
+  elapsed = ((double)t2 - t1) / CLOCKS_PER_SEC * 1000;
+  return elapsed;
+}
 int main(int argc, char** argv){ 
-  ll cap_duration = 1000000000;
+  ll cap_duration = 1000;
+  const int cur_camera = 0;
+
+
+  VideoCapture cap;
+  if(!cap.open(cur_camera)) return 0;
   if(argc == 1) std::cout<<"no time specified defaulting to 1 second"<<std::endl;
   else if(argc > 2) std::cout<<"too many times specified defaulting to 1 second"<<std::endl;
   else{
-    cap_duration = ((ll)argv[1])*1000000000;
+    cap_duration = ((ll)argv[1])*1000;
   }
 
-  struct timespec end_time, cur_time;
-  clock_gettime(CLOCK_MONOTONIC,&end_time);
-  end_time.tv_nsec += cap_duration;
-  while(clock_gettime(CLOCK_MONOTONIC,&cur_time).tv_nsec <end_time.tv_nsec){
-    
+  clock_t start_time, cur_time;
+  start_time = clock();
+  cur_time = clock();
+  Mat frame;
+  while(timediff(start_time, cur_time) <= cap_duration){
+    //if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+    std::cout<<cur_time<<std::endl;
+    waitKey(10);
+    cap.read(frame);
+    imshow("capturing",frame);
+    cur_time = clock();
   }
 
   return 0; 
