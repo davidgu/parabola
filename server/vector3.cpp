d29b2a2e12b4dd994a85fd54aeae2736a5d2ff7c
David Gu <david.gu@hotmail.com>
2018-11-11 02:20:52 -0500

    Fixed problem with plus and minus operators

--- server/vector3.cpp
+++ server/vector3.cpp
@@ -6,10 +6,10 @@ Vector3::Vector3(double a, double b, double c){
   z = c;
 }
 Vector3 operator+(const Vector3& a, const Vector3& b){
-  return Vector3(a.x + b.x, a.y + b.y, a.z + a.z);
+  return Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 Vector3 operator-(const Vector3& a, const Vector3& b){
-  return Vector3(a.x - b.x, a.y - b.y, a.z - a.z);
+  return Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 Vector3 operator/(const Vector3& a, double b){
   return Vector3(a.x/b, a.y/b, a.z/b);
