c16b8096b390fd06a4eebf39373e33aa082bf40c
David Gu <david.gu@hotmail.com>
2018-11-11 02:07:27 -0500

    Fixed overloaded operators

--- server/vector3.cpp
+++ server/vector3.cpp
@@ -1,21 +1,21 @@
 #include "vector3.h"
-#include <stdexcept>
+
 Vector3::Vector3(double a, double b, double c){
   x = a;
   y = b;
   z = c;
 }
-Vector3 Vector3::operator+(const Vector3& a, const Vector3& b){
+Vector3 operator+(const Vector3& a, const Vector3& b){
   return Vector3(a.x + b.x, a.y + b.y, a.z + a.z);
 }
-Vector3 Vector3::operator-(const Vector3& a, const Vector3& b){
+Vector3 operator-(const Vector3& a, const Vector3& b){
   return Vector3(a.x - b.x, a.y - b.y, a.z - a.z);
 }
-Vector3 Vector3::operator/(Vector3& a, double b){
-  return Vector3(a.x/b, a.y/b, a.z/b)
+Vector3 operator/(const Vector3& a, double b){
+  return Vector3(a.x/b, a.y/b, a.z/b);
 }
-Vector3 Vector3::operator*(Vector3& a, double b){
-  return Vector3(a.x*b, a.y*b, a.z*b)
+Vector3 operator*(const Vector3& a, double b){
+  return Vector3(a.x*b, a.y*b, a.z*b);
 }
 
 double Vector3::dot(const Vector3 a, const Vector3 b){
