0a4701ab945ee277c15783c84bb598cc0e2db5ac
David Gu <david.gu@hotmail.com>
2018-11-11 02:10:53 -0500

    Fixed tests, now all tests are correct and pass

--- server/vector3_test.cpp
+++ server/vector3_test.cpp
@@ -1,21 +1,26 @@
 #include "vector3.h"
 #include <assert.h>
+#include <iostream>
 
 int main(){
   Vector3 v1 = Vector3(2.0,3.0,4.0);
   Vector3 v2 = Vector3(2.0,3.0,4.0);
-  //arithmertic operators
+
+  // Equality
+  assert(v1==v2);
+
+  // Arithmertic operators
   assert(v1+v2 == Vector3(4.0,6.0,8.0));
   assert(v1-v2 == Vector3(0.0,0.0,0.0));
-  assert(v1*v2 == Vector3(4.0,9.0,16.0));
-  assert(v1/v2 == Vector3(1.0,1.0,1.0));
+  assert(v1*2 == Vector3(4.0,6.0,8.0));
+  assert(v1/2 == Vector3(1.0,1.5,2.0));
 
-  //dot()
-  assert(dot() == 28.0);
+  // dot()
+  assert(Vector3::dot(v1, v2) == 29.0);
 
-  //sum_components_squared()
-  assert(sum_components_squared(v1) == 4.0 + 9.0 + 16.0);
-
-  //projection()
+  //proj()
+  Vector3 v3 = Vector3(-1.0,4.0,2.0);
+  Vector3 v4 = Vector3(1.0,0.0,3.0);
+  assert(Vector3::proj(v3, v4) == Vector3(0.5, 0.0, 1.5));
 
 }
