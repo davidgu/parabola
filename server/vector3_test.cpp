d541efa89a26fe484f5e780eafdb6230dc5b18bf
David Gu <david.gu@hotmail.com>
2018-11-11 02:21:17 -0500

    Added tests for vector perp

--- server/vector3_test.cpp
+++ server/vector3_test.cpp
@@ -18,9 +18,9 @@ int main(){
   // dot()
   assert(Vector3::dot(v1, v2) == 29.0);
 
-  //proj()
+  // proj() & perp()
   Vector3 v3 = Vector3(-1.0,4.0,2.0);
   Vector3 v4 = Vector3(1.0,0.0,3.0);
   assert(Vector3::proj(v3, v4) == Vector3(0.5, 0.0, 1.5));
-
+  assert(Vector3::perp(v3, v4) == Vector3(0.5, 0.0, 1.5));
 }
