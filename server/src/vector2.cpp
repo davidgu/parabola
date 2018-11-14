befcdf9ddd4fa1afbcc1ae530e65e54503580d8f
David Gu <david.gu@hotmail.com>
2018-11-14 16:07:02 -0500

    Added default constructor to Vector2

--- server/src/vector2.cpp
+++ server/src/vector2.cpp
@@ -5,6 +5,11 @@ Vector2::Vector2(double a, double b){
   y = b;
 }
 
+Vector2::Vector2(){
+  x = 0.0;
+  y = 0.0;
+}
+
 Vector2 operator+(const Vector2& a, const Vector2& b){
   return Vector2(a.x + b.x, a.y + b.y);
 }
