0c10c291925a76489a389b041fb7303e4fb1a282
David Gu <david.gu@hotmail.com>
2018-11-14 16:06:43 -0500

    Fixed bugs in cameraconfig header

--- server/src/cameraconfig.hpp
+++ server/src/cameraconfig.hpp
@@ -1,21 +1,22 @@
 #ifndef CAMERACONFIG_H
 #define CAMERACONFIG_H
 
+#include <string>
+
 #include "opencv2/calib3d.hpp"
 #include "vector3.hpp"
-#include <string>
 
 class CameraConfig{
     private:
-        const int index;
-        const Vector3 position;
+        int index;
+        Vector3 position;
         // This will contain more data about the camera, such as dewarp
         // parameters.
     public:
         CameraConfig(int i, Vector3 p);
-        void load_from_file(std::string)
         int get_index();
         Vector3 get_position();
+        void load_from_file(std::string path);
 };
 
 #endif
