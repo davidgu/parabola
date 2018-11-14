d141cf336c54d2371c21fd4786db6b12e5edf131
David Gu <david.gu@hotmail.com>
2018-11-14 16:07:09 -0500

    Fixed Vector2 header

--- server/src/vector2.hpp
+++ server/src/vector2.hpp
@@ -1,11 +1,11 @@
-#ifndef VECTOR3_H
-#define VECTOR3_H
+#ifndef VECTOR2_H
+#define VECTOR2_H
 
 class Vector2{
-  private:
-    double x, y;
   public:
+    double x, y;
     Vector2(double a, double b);
+    Vector2();
     friend Vector2 operator+(const Vector2& a, const Vector2& b);
     friend Vector2 operator-(const Vector2& a, const Vector2& b);
 };
