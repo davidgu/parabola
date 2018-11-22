522b378d382acf6e11a07950473b4ee52e6744c6
Curtis Chong <curtischong5@gmail.com>
2018-11-22 01:52:43 -0500

    fixed cone detection

--- server/src/start.cpp
+++ server/src/start.cpp
@@ -25,7 +25,7 @@ double sideCamHeights = 1.4;
 // 2 -> East camera
 Vector3 camWPoss[3];
 
-int camIdx[3];
+int idxToCam[4];
 int rotCamIdx[3];
 int coneCali[2][3];
 int ballCali1[2][3];
@@ -55,7 +55,7 @@ void read_config_file(){
   infile>> eastCameraY;
   camWPoss[2] = Vector3(0, eastCameraY, eastCameraZ);
 
-  infile>>camIdx[0]>>camIdx[1]>>camIdx[2];
+  //infile>>idxToCam[0]>>idxToCam[1]>>idxToCam[2];
   infile>>rotCamIdx[0]>>rotCamIdx[1]>>rotCamIdx[2];
   infile>>coneCali[0][0]>>coneCali[0][1]>>coneCali[0][2]>>coneCali[1][0]>>coneCali[1][1]>>coneCali[1][2];
   infile>>ballCali1[0][0]>>ballCali1[0][1]>>ballCali1[0][2]>>ballCali1[1][0]>>ballCali1[1][1]>>ballCali1[1][2];
@@ -63,6 +63,63 @@ void read_config_file(){
   infile>>topRotTheta;
 }
 
+Mat rotateFrame(Mat curFrame, int curCam){
+    Point center = cv::Point( curFrame.cols/2, curFrame.rows/2 );
+    double scale = 1.0;
+    std::cout<<"rotation "<<curCam<<" "<<rotCamIdx[curCam]<<std::endl;
+    Mat rot_mat = cv::getRotationMatrix2D( center, rotCamIdx[curCam], scale);
+    Mat res;
+    warpAffine( curFrame, res, rot_mat, curFrame.size() );
+    return res;
+}
+
+void configure_cameras(){
+  Mat frame;
+
+  // internal camera index to opencv camera index
+  for(int i = 0; i<4; i++){
+    if(!capArr[i].open(i)){
+      std::cout<<"can't open cam: "<<i<<std::endl;
+    }
+    capArr[i].set(CV_CAP_PROP_FRAME_WIDTH,640);
+    capArr[i].set(CV_CAP_PROP_FRAME_HEIGHT,480);
+    capArr[i].set(CV_CAP_PROP_AUTOFOCUS, 0);
+    capArr[i].set(CV_CAP_PROP_EXPOSURE, 0);
+    while(true){
+      capArr[i].read(frame);
+      putText(frame, "Please select the cam ID", cv::Point2f(50,100), cv::FONT_HERSHEY_DUPLEX, 1,  cv::Scalar(0,0,255), 2);
+      int key = cv::waitKey(10);
+      //if(key !=-1) std::cout<<key<<std::endl;
+      // Zero key - This is always Top 
+      if(key == 48){
+        idxToCam[0] = i;
+        break;
+      }
+
+      // One key - This is always North
+      if(key == 49){
+        idxToCam[1] = i;
+        break;
+      }
+
+      // Two key - This is always East
+      if(key == 50){
+        idxToCam[2] = i;
+        break;
+      }
+
+      // Three key - This should always be the webcam
+      if(key == 51){
+        idxToCam[3] = i;
+        break;
+      }
+      imshow("cam", frame);
+    }
+  }
+  destroyWindow("cam");
+  // Waitkey must be here to trigger window destroy
+  cv::waitKey(1);
+}
 Vector2 fix_top_angle(Vector2 ballLoc){
   Vector2 ans;
   // here we are multiplying the location vector by a rotation matrix
@@ -74,8 +131,8 @@ Vector2 fix_top_angle(Vector2 ballLoc){
 Mat detect_cones(Mat frame){
   Mat hsv;
   cvtColor(frame, hsv, CV_BGR2HSV);
-  Scalar orange_lower(coneCali[0][0], coneCali[0][1], coneCali[0][2]); 
-  Scalar orange_upper(coneCali[1][0], coneCali[1][1], coneCali[1][2]);
+  Scalar orange_lower(1,180,210); // try 5 and 10 for the first index . that works pretty well // 2 200, 150
+  Scalar orange_upper(15,255,255);
   Mat orange_mask;
   inRange(hsv, orange_lower, orange_upper,orange_mask);
   return orange_mask;
@@ -108,6 +165,7 @@ Mat findBiggestBlob(Mat & matImage){
 Vector2 detect_ball(Mat frame, bool *success){
   GaussianBlur( frame, frame, Size(11, 11), 4, 4);
   Mat final_image = detect_cones(frame);
+  imshow("asdsa",final_image);
   final_image = findBiggestBlob(final_image);
 
   //start analyzing the image
@@ -128,20 +186,26 @@ Vector2 detect_ball(Mat frame, bool *success){
     *success = false;
   }
 
+  circle(frame, p1, 6, Scalar(256,256,256), -1); // plot the point of the ball
+
+
   // you can make it return a vector 2 of the location of the ball:
   return Vector2(x,y);
 }
 
 Vector2 cam_read_frame_pos(int idx, bool* success){
   Vector2 ballLoc;
-  capArr[camIdx[idx]].read(frames[idx]);
+  capArr[idxToCam[idx]].read(frames[idx]);
+  //frames[idx] = rotateFrame(frames[idx],idx);
   bool detectSuccess = false;
   ballLoc = detect_ball(frames[idx], &detectSuccess);
   if(detectSuccess){
     *success = true;
+    std::cout<<"Camera "<<idx<<" DETECT"<<std::endl;
   }
   else{
     *success = false;
+    std::cout<<"Camera "<<idx<<" NO DETECT"<<std::endl;
   }
   return ballLoc;
 }
@@ -157,6 +221,8 @@ int main(){
   read_config_file();
 
   std::cout<<"Initializing Cameras"<<std::endl;
+  configure_cameras(); 
+  /*
   for(int i = 0 ; i < 4; i++){
     if(!capArr[i].open(i)){
       std::cout<<"can't open cam: "<<i<<std::endl;
@@ -165,7 +231,7 @@ int main(){
     capArr[i].set(CV_CAP_PROP_FRAME_HEIGHT,480);
     capArr[i].set(CV_CAP_PROP_AUTOFOCUS, 0);
     capArr[i].set(CV_CAP_PROP_EXPOSURE, 0);
-  }
+  }*/
 
   // Start simclock
   simClock = SimClock();
@@ -200,13 +266,15 @@ int main(){
     // This is the two camera case
     else{
       bool success = false;
-      for(int i = 0; i < 2; i ++){
+      for(int i = 1; i < 2; i ++){
+        int key = waitKey(10);
         Vector2 bpos = cam_read_frame_pos(i, &success);
         // If detection fails for one of the cameras, skip this set
         if(!success){
-          break;
+          //break;
         }
         lines[i] = build_vector(i, camWPoss[i], bpos);
+        //std::cout << "Camera "<<i<<" ~ point: "<<lines[i].first<<", vec: "<<lines[i].second<<std::endl;
       }
 
       // If all cameras sucessfully detected the ball
