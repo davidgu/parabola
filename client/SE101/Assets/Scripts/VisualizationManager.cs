c8f4605ac4f9423c9aaa3a7c43ea608cff7be191
David Gu <david.gu@hotmail.com>
2018-11-21 21:57:01 -0500

    Included additional imports

--- client/SE101/Assets/Scripts/VisualizationManager.cs
+++ client/SE101/Assets/Scripts/VisualizationManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using WebSocketSharp;
 
 public class VisualizationManager : MonoBehaviour {
 
