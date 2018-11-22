c26c8a2b7db197906328c07445fc92e9f8bd0e63
Curtis Chong <curtischong5@gmail.com>
2018-11-22 04:01:33 -0500

    created day cone mask

--- server/src/debug.cpp
+++ server/src/debug.cpp
@@ -27,6 +27,27 @@ Mat detect_cones(Mat frame){
   return orange_mask;
 }
 
+Mat detect_cones_day(Mat frame){
+  GaussianBlur( frame, frame, Size(9, 9), 4, 4 );
+  frame = frame +  Scalar(-130, -130, -130);
+  //frame.convertTo(frame, -1, , 0);
+  //read input
+  //   GaussianBlur( frame, frame, Size(11, 11), 4, 4 );
+  Mat hsv;
+  //note hsv range is from [0,179], [0,255], [0,255] (Hue, Saturation, Value)
+  cvtColor(frame, hsv, CV_BGR2HSV);
+  Scalar orange_lower1(0,50,50); // try 5 and 10 for the first index . that works pretty well // 2 200, 150
+  Scalar orange_upper1(10,255,255);
+
+  Scalar orange_lower2(170,30,30); // try 5 and 10 for the first index . that works pretty well // 2 200, 150
+  Scalar orange_upper2(179,255,255);
+  Mat orange_mask;
+  inRange(hsv, orange_lower1, orange_upper1,orange_mask);
+  erode(orange_mask, orange_mask, Mat(), Point(-1, -1), 2, 1, 1);
+
+  return orange_mask;
+}
+
 
 
 Mat findBiggestBlob(Mat & matImage){
@@ -58,40 +79,20 @@ Mat findBiggestBlob(Mat & matImage){
 void custom_process_frame(Mat frame){
 
   //note hsv range is from [0,179], [0,255], [0,255] (Hue, Saturation, Value)
-  GaussianBlur( frame, frame, Size(11, 11), 4, 4 );
+  //GaussianBlur( frame, frame, Size(11, 11), 4, 4 );
   //frame = frame +  Scalar(-150, -150, -150);
   //frame.convertTo(frame, -1, 1.5, 0); //increase the contrast by the middle number
-  Mat hsv, hsv2;
-  cvtColor(frame, hsv, CV_BGR2HSV);
-  hsv.convertTo(hsv2, -1, 2, 0); //increase the contrast by the middle number
-  //in dimmer environments the ball looks more orange and this is the mask for it
-  Scalar purple_lower1(0,100,100); // I find that the ball isn't as affected by the middle number as other numbers. it is so high just to filter out the other colours
-  Scalar purple_upper1(10,255,255); // limit the 255 if I don't want to orange cone. It's pretty bad cause they occupy almost the same hsv space
 
-  Scalar purple_lower2(105,80,80);
-  Scalar purple_upper2(179,255,255);
-
-  //in brighter environments the ball looks more pink and this is the mask for it
-  Mat purple_mask;
-  Mat purple_mask1;
-  Mat purple_mask2;
-  inRange(hsv, purple_lower1, purple_upper1, purple_mask1);
-  inRange(hsv, purple_lower2, purple_upper2, purple_mask2);
-
-  addWeighted(purple_mask1, 1.0, purple_mask2, 1.0, 0.0, purple_mask);
-
-  Mat final_image;
-  purple_mask.convertTo(final_image, -1, 4, 0); //increase the contrast by the middle number
-  final_image = detect_cones(frame);
+  Mat final_image = detect_cones_day(frame);
   //remove the cones from the mask
-  Mat coneMask = detect_cones(frame);
+  //Mat coneMask = detect_cones(frame);
   //imshow("asd",coneMask);
 
-  dilate(coneMask, coneMask, Mat(), Point(-1, -1), 2, 1, 1);
-  dilate(coneMask, coneMask, Mat(), Point(-1, -1), 2, 1, 1);
-  dilate(coneMask, coneMask, Mat(), Point(-1, -1), 2, 1, 1);
+  //dilate(coneMask, coneMask, Mat(), Point(-1, -1), 2, 1, 1);
+  //dilate(coneMask, coneMask, Mat(), Point(-1, -1), 2, 1, 1);
+  //dilate(coneMask, coneMask, Mat(), Point(-1, -1), 2, 1, 1);
   //we don't want to fill in the contours here because it might cover up the ball
-  coneMask = ~coneMask;
+  //coneMask = ~coneMask;
   //final_image = final_image & coneMask;
 
   //start analyzing the image
@@ -133,7 +134,7 @@ int main(int argc, char** argv){
     capArr[i].set(CV_CAP_PROP_EXPOSURE, 0);
   }
   std::cout<<"Reading Frames"<<std::endl;
-  int curCam = 0;
+  int curCam = 3;
   int curRot[4] = {0};
   while(true){
     int key = waitKey(10);
